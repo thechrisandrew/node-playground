cd54430f32f04f835e072d72d762dcb353da9e72
thechrisandrew <christopherandrew612@gmail.com>
2022-10-20 23:01:42 +0700

    fix: Update validators and authController
    * register and login now uses validated result for data consistency
    * register and login validator now returns validatedValue

--- jwt-auth-api-express-prisma/src/controllers/authController.ts
+++ jwt-auth-api-express-prisma/src/controllers/authController.ts
@@ -12,7 +12,7 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
     const { username, email, password, confirmPassword } = req.body;
 
     // validate user inputs using joi
-    const error = validateRegisterInput(username, email, password, confirmPassword);
+    const { error, validatedValue } = validateRegisterInput(username, email, password, confirmPassword);
     if (error) {
         // return res.status(400).send({ message: "Invalid user inputs provided" });
         return next(createError(400, "Invalid user input(s) provided"));
@@ -20,20 +20,20 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
     // checks if user exists on database
     const user = await prisma.user.findFirst({
-        where: { OR: [{ username }, { email }] },
+        where: { OR: [{ username: validatedValue.username }, { email: validatedValue.email }] },
     });
     if (user) {
         return next(createError(409, "This username or email has already been registered"));
     }
 
     // hash password with bcrypt before storing
-    const hashedPassword = await hash(password, 12);
+    const hashedPassword = await hash(validatedValue.password, 12);
 
     // saves the user to the database
     const result = await prisma.user.create({
         data: {
-            username,
-            email,
+            username: validatedValue.username,
+            email: validatedValue.email,
             password: hashedPassword,
         },
     });
@@ -44,7 +44,7 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
     const { username, password } = req.body;
 
     // validate user inputs using joi
-    const error = validateLoginInput(username, password);
+    const { error, validatedValue } = validateLoginInput(username, password);
     if (error) {
         // return res.status(400).send({ message: "Invalid user inputs provided" });
         return next(createError(400, "Invalid user input(s) provided"));
@@ -52,14 +52,14 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
 
     // finds user in database
     const user = await prisma.user.findUnique({
-        where: { username },
+        where: { username: validatedValue.username },
     });
     if (!user) {
         return next(createError(401, "Invalid credentials provided"));
     }
 
     // checks if password is correct
-    const match = await compare(password, user.password);
+    const match = await compare(validatedValue.password, user.password);
     if (!match) {
         return next(createError(401, "Invalid credentials provided"));
     }
--- jwt-auth-api-express-prisma/src/utils/validators.ts
+++ jwt-auth-api-express-prisma/src/utils/validators.ts
@@ -2,26 +2,29 @@ import Joi from "joi";
 
 export const validateRegisterInput = (username: string, email: string, password: string, confirmPassword: string) => {
     const registerSchema = Joi.object().keys({
-        username: Joi.string().alphanum().min(6).max(30).required().trim(),
-        email: Joi.string().email().required().trim(),
+        username: Joi.string().alphanum().min(6).max(30).required().trim().lowercase(),
+        email: Joi.string().email().required().trim().lowercase(),
         password: Joi.string().trim().min(8).required(),
         confirmPassword: Joi.string().trim().valid(Joi.ref("password")).required().messages({
             "any.only": `"password" must match`,
         }),
     });
 
-    const { error } = registerSchema.validate({ username, email, password, confirmPassword }, { abortEarly: false });
+    const { error, value } = registerSchema.validate(
+        { username, email, password, confirmPassword },
+        { abortEarly: false }
+    );
 
-    return error;
+    return { error, validatedValue: value };
 };
 
 export const validateLoginInput = (username: string, password: string) => {
     const loginSchema = Joi.object().keys({
-        username: Joi.string().alphanum().min(6).max(30).required().trim(),
+        username: Joi.string().alphanum().min(6).max(30).required().trim().lowercase(),
         password: Joi.string().trim().min(8).required(),
     });
 
-    const { error } = loginSchema.validate({ username, password });
+    const { error, value } = loginSchema.validate({ username, password }, { abortEarly: false });
 
-    return error;
+    return { error, validatedValue: value };
 };
